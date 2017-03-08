f088e1cc33d0f453b0fe7133eb52de2082035395
Ngarnier <ng161621@edu.ecetech.fr>
2017-03-08 15:45:04 +0100

    bottom

--- index.php
+++ index.php
@@ -36,7 +36,9 @@
 
     <!-- Header -->
     <header id="top" class="header">
-
+        <div class="text-vertical-center">
+            <a href="#about" class="btn btn-dark btn-lg">Découvrir</a>
+        </div>
     </header>
 
     <!-- About -->
