8c9bc4d6c03b064f494c1f6eb5552e8c5224ec9e
antoinemaschi <antoine.maschi@edu.ecetech.fr>
2017-03-15 10:17:45 +0100

    maj
    img ow, img hs, logo lol, nom+slogan

--- index.php
+++ index.php
@@ -46,8 +46,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
-                    <p class="lead">This theme features some wonderful photography courtesy of <a target="_blank" href="http://join.deathtothestockphoto.com/">Death to the Stock Photo</a>.</p>
+                    <h2>E-Sport Gaming Enterprise</h2>
+                    <p class="lead">Be the <a target="_blank" href="#tournois">Player</a>.</p>
                 </div>
             </div>
             <!-- /.row -->
